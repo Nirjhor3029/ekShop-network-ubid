7e092746eac1ea4626435708ce044ca413c8913a
Nirjhor3029 <sazzad3029@gmail.com>
2023-09-17 15:24:41 +0600

    number format or not check

--- app/Http/Controllers/Api/Inbound/NumberFormatController.php
+++ app/Http/Controllers/Api/Inbound/NumberFormatController.php
@@ -4,29 +4,43 @@ namespace App\Http\Controllers\Api\Inbound;
 
 use App\Http\Controllers\Controller;
 use App\Models\BusinessListing;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class NumberFormatController extends Controller
 {
     public function formatMobileNumbers()
     {
-        $bsnsListings = BusinessListing::all();
+        $bsnsListings = BusinessListing::where('is_number_format', 0)
+            ->get();
 
-        foreach ($bsnsListings as $key => $item) {
-            // echo $item->mobile;
-            // return $item;
-            $item->mobile = $this->onlyPhoneNumber($item->mobile);
-            $item->business_mobile = $this->onlyPhoneNumber($item->business_mobile);
-            $item->business_owner_mobile = $this->onlyPhoneNumber($item->business_owner_mobile);
-            $item->business_contact_person_mobile = $this->onlyPhoneNumber($item->business_contact_person_mobile);
-            $item->business_owner1_mobile = $this->onlyPhoneNumber($item->business_owner1_mobile);
-            $item->business_owner2_mobile = $this->onlyPhoneNumber($item->business_owner2_mobile);
-            $item->business_owner3_mobile = $this->onlyPhoneNumber($item->business_owner3_mobile);
-            $item->save();
+        if (count($bsnsListings)) {
+            foreach ($bsnsListings as $key => $item) {
+                // echo $item->mobile;
+                // return $item;
+                $item->mobile = $this->onlyPhoneNumber($item->mobile);
+                $item->business_mobile = $this->onlyPhoneNumber($item->business_mobile);
+                $item->business_owner_mobile = $this->onlyPhoneNumber($item->business_owner_mobile);
+                $item->business_contact_person_mobile = $this->onlyPhoneNumber($item->business_contact_person_mobile);
+                $item->business_owner1_mobile = $this->onlyPhoneNumber($item->business_owner1_mobile);
+                $item->business_owner2_mobile = $this->onlyPhoneNumber($item->business_owner2_mobile);
+                $item->business_owner3_mobile = $this->onlyPhoneNumber($item->business_owner3_mobile);
+                $item->is_number_format = 1;
+                $item->save();
 
-            echo $item->mobile . "<br>";
+                echo $item->mobile . " ";
+            }
+
+            return response()->json([
+                'status' => 200,
+                'message' => ($key+1) . " mobile numbers format successfully"
+            ]);
+        } else {
+            return response()->json([
+                'status' => 200,
+                'message' => "All mobile numbers already formatted."
+            ]);
         }
-
         // return $this->onlyPhoneNumber("+8801685-262326");
     }
 
